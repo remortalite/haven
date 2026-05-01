8e01cb3e2a51a603f2e7e7c6a0c1f808aaeec421
Rem Mora <remortalite@gmail.com>
2026-05-01 14:41:52 +0700

    [+] add custom reactions (kinda sketchy but ok)

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,9 +1,9 @@
 class LikesController < ApplicationController
   def create
     post = Post.find(params[:post_id])
-    @like = post.likes.create(user_id: current_user.id)
+    @like = post.likes.create(user_id: current_user.id, reaction: params[:reaction] || "💜")
     @like.save!
-    redirect_to post, notice: "You have liked this post"
+    redirect_to post, notice: "Спасибо за реакцию!"
 #    redirect_back(fallback_location: post)
   end
 
@@ -13,7 +13,7 @@ class LikesController < ApplicationController
     if (@like.user.id == current_user.id || current_user.admin == 1)
       @like.destroy
     end
-    redirect_to post, notice: "Your like has been removed from this post"
+    redirect_to post, notice: "Кажется вы убрали реакцию..."
 #    redirect_back(fallback_location: post)
   end
 end
